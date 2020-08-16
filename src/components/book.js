00dddc3b7b866d99ee74de6b6a1975ee1aa2f2f0
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-08-16 09:19:28 +0300

    Fix linter error - usage of singlequote

--- src/components/book.js
+++ src/components/book.js
@@ -1,48 +1,48 @@
 /* eslint-disable react/forbid-prop-types */
-import React from "react"
-import PropTypes from "prop-types"
-import "../style.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../style.css';
 
 const Book = (props) => {
-  const { book } = props
-  const { id, title, category } = book
-  const { onClick } = props
+  const { book } = props;
+  const { id, title, category } = book;
+  const { onClick } = props;
   const removeBook = () => {
-    onClick(book)
-  }
+    onClick(book);
+  };
   return (
-    <div className='book' id={`book${id}`}>
-      <div className='book-name'>
-        <p className='category'>{category}</p>
-        <p className='title'>{title}</p>
-        <button className='button' type='button'>
+    <div className="book" id={`book${id}`}>
+      <div className="book-name">
+        <p className="category">{category}</p>
+        <p className="title">{title}</p>
+        <button className="button" type="button">
           Comment
         </button>
-        <span className='opacity-val'> | </span>
-        <button className='button' type='button' onClick={removeBook}>
+        <span className="opacity-val"> | </span>
+        <button className="button" type="button" onClick={removeBook}>
           Remove
         </button>
-        <span className='opacity-val'> | </span>
-        <button className='button' type='button'>
+        <span className="opacity-val"> | </span>
+        <button className="button" type="button">
           Edit
         </button>
       </div>
-      <div className='flex-display'>
-        <div className='progress-bar flex-display'>
-          <div className='completition-bar'></div>
-          <div className='completed-progress'>
-            <p className='percentage '>75%</p>
-            <p className='completed opacity-val'>Completed</p>
+      <div className="flex-display">
+        <div className="progress-bar flex-display">
+          <div className="completition-bar"/>
+          <div className="completed-progress">
+            <p className="percentage ">75%</p>
+            <p className="completed opacity-val">Completed</p>
           </div>
         </div>
         <div>
-          <div className='vertical-line opacity-val'></div>
+          <div className="vertical-line opacity-val"/>
           <div>
-            <p className='progress-title opacity-val margin-botton'>
+            <p className="progress-title opacity-val margin-botton">
               CURRENT CHAPTER
             </p>
-            <p className='.current-lesson margin-botton'>Chapter: 43</p>
-            <button className='update-progress-button'>UPDATE PROGRESS</button>
+            <p className=".current-lesson margin-botton">Chapter: 43</p>
+            <button className="update-progress-button">UPDATE PROGRESS</button>
           </div>
         </div>
       </div>
