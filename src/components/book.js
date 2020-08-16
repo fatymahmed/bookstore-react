53232f2e3e8e5f295b3cb7fa5c0e7cd9c82ce39b
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-08-16 08:57:11 +0300

    Add placeholders for update progress

--- src/components/book.js
+++ src/components/book.js
@@ -1,27 +1,58 @@
 /* eslint-disable react/forbid-prop-types */
-import React from 'react';
-import PropTypes from 'prop-types';
-import '../style.css';
+import React from "react"
+import PropTypes from "prop-types"
+import "../style.css"
 
 const Book = (props) => {
-  const { book } = props;
-  const { id, title, category } = book;
-  const { onClick } = props;
+  const { book } = props
+  const { id, title, category } = book
+  const { onClick } = props
   const removeBook = () => {
-    onClick(book);
-  };
+    onClick(book)
+  }
   return (
-    <div className="book" id={`book${id}`}>
-      <p className="category">{category}</p>
-      <p className="title">{title}</p>
-      <button className="button" type="button" onClick={removeBook}>Remove book</button>
+    <div className='book' id={`book${id}`}>
+      <div className='book-name'>
+        <p className='category'>{category}</p>
+        <p className='title'>{title}</p>
+        <button className='button' type='button'>
+          Comment
+        </button>
+        <span className='opacity-val'> | </span>
+        <button className='button' type='button' onClick={removeBook}>
+          Remove
+        </button>
+        <span className='opacity-val'> | </span>
+        <button className='button' type='button'>
+          Edit
+        </button>
+      </div>
+      <div className='flex-display'>
+        <div className='progress-bar flex-display'>
+          <div className='completition-bar'></div>
+          <div className='completed-progress'>
+            <p className='percentage '>75%</p>
+            <p className='completed opacity-val'>Completed</p>
+          </div>
+        </div>
+        <div>
+          <div className='vertical-line opacity-val'></div>
+          <div>
+            <p className='progress-title opacity-val margin-botton'>
+              CURRENT CHAPTER
+            </p>
+            <p className='.current-lesson margin-botton'>Chapter: 43</p>
+            <button className='update-progress-button'>UPDATE PROGRESS</button>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-};
+  )
+}
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
   onClick: PropTypes.func.isRequired,
-};
+}
 
-export default Book;
+export default Book
--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React from "react"
 
 const Header = () => (
-  <div className="header-container">
-    <div className="header">
-      <h2 className="bookstore">
-Bookstore CMS
-      </h2>
-      <h4 className="books">BOOKS</h4>
-      <h4 className="categories">CATEGORIES</h4>
+  <div className='header-container'>
+    <div className='header'>
+      <h1 className='bookstore'>Bookstore CMS</h1>
+      <h4 className='books'>BOOKS</h4>
+      <h4 className='categories'>CATEGORIES</h4>
     </div>
-
   </div>
+)
 
-);
-
-export default Header;
+export default Header
